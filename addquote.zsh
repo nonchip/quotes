fb3f47e7ee290ee090fb761bf3b51cbed41f25e3
Kyra Zimmer <me@nonchip.de>
2015-05-17 10:44:40 +0200

    improved system

--- addquote.zsh
+++ addquote.zsh
@@ -1,9 +1,12 @@
 #!/bin/zsh
+cd "$(dirname "$(readlink -f "$0")")"
 
-cat >> quotes.raw
-echo "%" >> quotes.raw
-git add quotes.raw
-git commit -am "[addquote] ${1:=new quote}"
+cat > .quotes.tmp
+fn="$(date +%s)-$(sha1sum .quotes.tmp | cut -d" " -f 1)"
+fp="quotes/${fn}.u8"
+mv .quotes.tmp $fp
+git add $fp
+git commit -am "[addquote] ${1:=$fn}"
 git push
+find quotes -name "*.u8" -exec cat {} \; -exec echo "%" \; > quotes.u8
 strfile quotes.u8
-strfile quotes
--- getquote.zsh
+++ getquote.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 git pull >&2
+find quotes -name "*.u8" -exec cat {} \; -exec echo "%" \; > quotes.u8
 strfile quotes.u8 >&2
-strfile quotes >&2
 fortune "$@" quotes.u8
